5bfaf120e0b6bb973c72d183f72cb296bfcecf44
Inwady <inwady@gmail.com>
2017-06-03 07:54:10 +0300

    Add listen

--- public/js/conf.js
+++ public/js/conf.js
@@ -54,7 +54,7 @@ window.conf = {
         {prefixSocket : "wss://", prefix: "https://", host: "cyclic-server.herokuapp.com", port: "", path: "/game"},
         {prefixSocket : "ws://", prefix: "http://", host: "172.16.94.65", port: ":8081", path: "/game"}
     ],
-    baseIP: 3,
+    baseIP: 5,
 
     // UI
     rectSize: 100,
